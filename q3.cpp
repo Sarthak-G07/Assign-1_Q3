ccc35dfd0150ebf4f985f0d7c2abda8f44e580a8
Sarthak-G07 <22103271@mail.jiit.ac.in>
2024-07-27 10:30:09 +0530

    Add files via upload

---
+++ q3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+
+    int start = 0;
+    int end = input.length() - 1;
+
+    while (start < end) {
+        swap(input[start], input[end]);
+        start++;
+        end--;
+    }
+
+    cout << "Reversed string: " << input << endl;
+    return 0;
+}
+
